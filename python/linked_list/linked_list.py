65e51c792f0ff1617b7e683eea92f36d44c4d6b9
sarazwairi <sara.zwairi@gmail.com>
2021-07-29 22:23:52 +0300

    8 pass

--- python/linked_list/linked_list.py
+++ python/linked_list/linked_list.py
@@ -1,3 +1,8 @@
+class Node:
+    def __init__(self,value=None):
+        self.value=value
+        self.next=None
+
 class LinkedList:
     """
     Put docstring here
@@ -5,8 +10,35 @@ class LinkedList:
 
     def __init__(self):
         # initialization here
-        pass
+        self.head=None
 
-    def some_method(self):
+    def insert(self,value):
         # method body here
-        pass
+        node=Node(value)
+        node.next=self.head
+        self.head=node
+
+    def includes(self,value):
+
+        current=self.head
+        while current:
+            if current.value==value:
+                return True
+            else :
+                current=current.next
+        return False
+
+    def __str__(self):
+        current=self.head
+        linkListStr=''
+        while True:
+            if current!=None:
+                linkListStr+="{"+str(current.value)+"}->"
+                current=current.next
+            elif current==None:
+                linkListStr+="Null"
+                break
+        return linkListStr
+
+   
+
